93c968912e46afeb0524bfa3f03fb5abc3441619
yennyelizabeth <yenny.elizabeth@hotmail.com>
2017-12-23 10:57:28 -0500

    añade

---
+++ js/app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+/* interactive(aqui hace arbol dom)-complete() */
+/* document - : loading, interactive(parseo ( etapa anterior)) , complete */
+/* si primero no carga no va hacer la carga del navegador */
+alert('estas');
+/*console.log(window.location.href);*/
+
+    setTimeout(function(){
+        window.location.href='views/register.html';
+    },5000);
+
+
+
+});
